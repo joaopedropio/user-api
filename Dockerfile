95a78da565955e025b32352f9204f868a6b4e629
Joao Pedro <joao-pio@hotmail.com>
2018-01-20 18:56:40 -0200

    Docker refactoring

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,6 @@
-FROM node:carbon
+FROM node:slim
 WORKDIR /usr/src/app
 COPY package*.json ./
 RUN npm install
 COPY . .
-EXPOSE 3000
 ENTRYPOINT [ "npm", "start" ]
--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 require('./db/db');
-//require('./app/controllers/users');
 const { port, domain } = require('./configs/app');
 const userRoutes = require('./app/routes/users');
 const statusRoutes = require('./app/routes/status');
