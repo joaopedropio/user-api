33fa1b684f461952cac91b80e75cc105487d2bc5
Joao Pedro Pio <joao-pio@hotmail.com>
2018-07-01 22:53:17 -0300

    Some refactoring

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-require('./db/db');
-
 const app = require('express')();
+const mongoose = require('mongoose');
+require('./app/models/users');
 const swaggerUi = require('swagger-ui-express');
 const bodyParser = require('body-parser');
 const routes = require('./app/routes');
@@ -11,4 +11,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/', routes)
 
 const { port, url } = require('./configs/app');
-app.listen(port, () => console.log(`Listening on ${url}`));
+const { dbURL } = require('./configs/db');
+mongoose.connect(dbURL).then(
+    () => {
+        app.listen(port, console.log(`Listening on port ${port}...`));
+    },
+    err => {
+        console.log(err);
+        process.exit();
+    }
+);
--- configs/db.js
+++ configs/db.js
@@ -1,4 +1,4 @@
-const domain = process.env.DB_DOMAIN || '127.0.0.1';
+const domain = process.env.DB_DOMAIN || 'mongo';
 const db = process.env.DB_NAME || 'UserAPI';
 
-exports.dbURI = `mongodb://${domain}/${db}`;
+exports.dbURL = `mongodb://${domain}/${db}`;
