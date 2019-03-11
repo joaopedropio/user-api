5eecad6b4c04c6a449d0a5ff133306eb26b50835
Joao Pedro Pio <joao-pio@hotmail.com>
2019-03-10 22:27:02 -0300

    fix

--- client/UserClientLibTest/UserClientTestes.cs
+++ client/UserClientLibTest/UserClientTestes.cs
@@ -17,14 +17,14 @@ namespace UserClientLibTest
             var stream = FileHelper.GetInputFile("avatar.png");
             var base64 = FileHelper.ImageToBase64(stream);
             var password = "password";
-            var user = new User("Joao Pedro", "joao@pedro.pio", "jpp", "endereco", "33323", base64);
+            var user = new User("Joao Pedro", "joao@pedro.pio", "jp", "endereco", "33323", base64);
 
             // Act
             await client.Post(user, password);
         }
 
         [TestMethod]
-        public async Task Should_ReturnAllUsers_When_GetUserIsCalled()
+        public async Task Should_ReturnUser_When_UserExists()
         {
             var user = await client.Get("jp");
             Assert.IsInstanceOfType(user, typeof(User));
@@ -39,7 +39,7 @@ namespace UserClientLibTest
         [TestMethod]
         public async Task Should_ChangePassword_When_OldPassordIsCorrect()
         {
-            await client.ChangePassword("jp", "senha", "senha");
+            await client.ChangePassword("jp", "password", "senha");
         }
         [TestMethod]
         public async Task Should_BeAuthentic_When_PasswordIsCorrent()
